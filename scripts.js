2e9f33f465af01877d04bd28f570ca227e8fb0e7
pmiasek <pmiasek@ce5.pl>
2017-08-30 12:43:28 +0200

    uzycie funkcji prompt

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
-// scripts.js
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
-
-var a = 8;
-var h = 5;
+var a = prompt('Wpisz wartość \'a\'');
+var h = prompt('Wpisz wartość \'h\'');;
 var triangleArea = a*h/2;
 
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+prompt('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ', + triangleArea);
