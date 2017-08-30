a8a99fd84c2f02ada8ea09593e9e7116960df2a8
pmiasek <pmiasek@ce5.pl>
2017-08-30 13:06:37 +0200

    zmiana na wyswietlanie wyniku w konsoli

--- scripts.js
+++ scripts.js
@@ -2,4 +2,4 @@ var a = prompt('Wpisz wartość \'a\'');
 var h = prompt('Wpisz wartość \'h\'');
 var triangleArea = a*h/2;
 
-prompt('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ', + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ', + triangleArea);
