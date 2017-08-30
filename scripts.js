a124521fc1c61a0bd78620ccc410b893ad01d19d
AgaMiasek <ag.miasek@gmail.com>
2017-08-30 09:49:14 +0200

    dodanie pliku readme

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+var a = 8;
+var h = 5;
+var triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
