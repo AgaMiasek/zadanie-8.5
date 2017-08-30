cc1b233defa4e49c65b691e1eaf346c843d59ee4
pmiasek <pmiasek@ce5.pl>
2017-08-30 12:46:02 +0200

    usuniecie zbednego srednika

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 var a = prompt('Wpisz wartość \'a\'');
-var h = prompt('Wpisz wartość \'h\'');;
+var h = prompt('Wpisz wartość \'h\'');
 var triangleArea = a*h/2;
 
 prompt('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ', + triangleArea);
